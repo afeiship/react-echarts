b20e147c1d7601dc9e54ec5d914f3e34eb25ac2e
afeiship <1290657123@qq.com>
2022-10-18 21:25:10 +0800

    feat: onReady + option

--- public/src/app.tsx
+++ public/src/app.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+// @ts-ignore
+import React, { useEffect, useState } from 'react';
 import ReactEcharts from '../../src/main';
 import styled from 'styled-components';
 
@@ -8,9 +9,40 @@ const Container = styled.div`
 `;
 
 export default (props: any) => {
+  const [instance, setInstance] = useState();
+  const option = {
+    title: {
+      text: 'ECharts 入门示例'
+    },
+    tooltip: {},
+    legend: {
+      data: ['销量']
+    },
+    xAxis: {
+      data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+    },
+    yAxis: {},
+    series: [
+      {
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }
+    ]
+  };
+
+  useEffect(() => {
+    console.log('instance: ', instance);
+    window['ins'] = instance;
+  }, [instance]);
+
   return (
     <Container>
-      <ReactEcharts />
+      <ReactEcharts
+        onReady={(e) => setInstance(e)}
+        style={{ width: '100%', height: 500, border: '1px solid blue' }}
+        option={option}
+      />
     </Container>
   );
 };
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,17 +1,18 @@
+import noop from '@jswork/noop';
 import classNames from 'classnames';
 import React, { Component } from 'react';
 import { loadScript } from '@jswork/loadkit';
-import { EChartsOption } from 'echarts';
+import { EChartsOption, ECharts } from 'echarts';
 
 const CLASS_NAME = 'react-echarts';
 
 declare global {
   interface Window {
-    echarts: any;
+    echarts: ECharts;
   }
 }
 
-export type ReactEchartsProps = {
+export type ReactEchartsProps = React.HTMLAttributes<HTMLDivElement> & {
   /**
    * The extended className for component.
    */
@@ -20,39 +21,49 @@ export type ReactEchartsProps = {
    * When the chart is ready, the callback will be called.
    * @param inEchartsInstance
    */
-  onInit: (inEchartsInstance: any) => void;
+  onReady?: (inEchartsInstance: ECharts) => void;
+  /**
+   * Echarts init options.
+   */
+  initOptions?: any;
   /**
    * The echarts options.
    */
-  option: EChartsOption
+  option?: EChartsOption;
 };
 
 export default class ReactEcharts extends Component<ReactEchartsProps> {
   static displayName = CLASS_NAME;
   static version = '__VERSION__';
-  static defaultProps = {};
+  static defaultProps = {
+    onReady: noop,
+    initOptions: {},
+    option: {}
+  };
 
   private rootRef = React.createRef<HTMLDivElement>();
 
   componentDidMount() {
-    const { onInit, option } = this.props;
-    loadScript('https://cdnjs.cloudflare.com/ajax/libs/echarts/5.4.0/echarts.min.js', { id: 'echarts' }).then(e => {
+    const { onReady, initOptions, option } = this.props;
+    loadScript('https://cdnjs.cloudflare.com/ajax/libs/echarts/5.4.0/echarts.min.js', {
+      id: 'echarts'
+    }).then((_) => {
       const echarts = window['echarts'];
-      const echartsInstance = echarts.init(this.rootRef.current);
-      echartsInstance.setOption(option);
-
-      onInit(echartsInstance);
+      const echartsInstance = echarts.init(this.rootRef.current!, initOptions);
+      echartsInstance.setOption(option!);
+      onReady!(echartsInstance);
     });
   }
 
   render() {
-    const { className, ...props } = this.props;
+    const { className, option, initOptions, onReady, ...props } = this.props;
     return (
       <div
         ref={this.rootRef}
         data-component={CLASS_NAME}
         className={classNames(CLASS_NAME, className)}
-        {...props} />
+        {...props}
+      />
     );
   }
 }
